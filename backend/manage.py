b9dac8ff2b08e548b324a52341a552ed89800800
Shevchenko Demid <co1omkooo@yandex.ru>
2025-02-12 15:51:09 +0300

    rename

--- backend/manage.py
+++ backend/manage.py
@@ -8,7 +8,7 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault(
         'DJANGO_SETTINGS_MODULE',
-        'footgram_backend.settings'
+        'foodgram_backend.settings'
     )
     try:
         from django.core.management import execute_from_command_line
