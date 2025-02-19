67a6ff9672d40e55e796985d1077341ffc5cc18b
Shevchenko Demid <co1omkooo@yandex.ru>
2025-02-19 17:39:08 +0300

    Update settings

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -10,8 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = True
-# DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1, localhost').split(', ')
 
