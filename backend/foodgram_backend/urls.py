3b7062b479e380ad7d642de38d6c8f1c1f221d85
Shevchenko Demid <co1omkooo@yandex.ru>
2025-02-20 11:23:03 +0300

    Fix

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -6,7 +6,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls', namespace='api')),
-    path('s/', include('recipes.urls', namespace='recipes')),
+    path('', include('recipes.urls', namespace='recipes')),
 ]
 
 if settings.DEBUG:
