72e1fb25b5bc54afc7a48b80a86a99877524fbea
Shevchenko Demid <co1omkooo@yandex.ru>
2025-02-20 11:52:05 +0300

    Fix

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -8,4 +8,4 @@ def redirect_short_link(request, pk):
     """Перенаправляет на полный URL рецепта."""
     if not Recipe.objects.filter(id=pk).exists():
         raise Http404(f'Рецепт c id {pk} не найден.')
-    return redirect('api:recipe-detail', args=[pk])
+    return redirect('api:recipe-detail', kwargs={'pk': pk})
